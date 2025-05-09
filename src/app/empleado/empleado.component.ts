0ff743c040c60118e56bd00169516b10cc629213
AaronPFDev <aaronpesqueirafarina@gmail.com>
2025-05-09 23:38:53 +0200

    Probando componentes inline

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.css']
+  // template: '<p>Aquí iría un empleado</p>',
+  styleUrls: ['./empleado.component.css'],
+  // styles: ['p{background-color: red;}'],
 })
-export class EmpleadoComponent {
-
-}
+export class EmpleadoComponent {}
